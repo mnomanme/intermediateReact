5fe01bb3a45d024b31382f8c170ca2ad9fee4ddb
nomanme <abdullah.noman083@gmail.com>
2021-09-12 01:01:25 +0600

    import react bootstrap

--- 36-react-bootstrap-material-UI-axios-rechart/test-material-ui/src/App.js
+++ 36-react-bootstrap-material-UI-axios-rechart/test-material-ui/src/App.js
@@ -1,7 +1,12 @@
+import MaterialButton from './components/MaterialButton/MaterialButton';
+import News from './components/News/News';
+
 const App = () => {
 	return (
 		<>
 			<h2>Material UI</h2>
+			<News />
+			<MaterialButton />
 		</>
 	);
 };
