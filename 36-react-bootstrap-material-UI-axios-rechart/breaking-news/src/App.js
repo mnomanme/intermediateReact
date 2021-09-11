7a1287ec26cca28e9efc53af848a168ec02995d7
nomanme <abdullah.noman083@gmail.com>
2021-09-11 23:22:28 +0600

    update

--- 36-react-bootstrap-material-UI-axios-rechart/breaking-news/src/App.js
+++ 36-react-bootstrap-material-UI-axios-rechart/breaking-news/src/App.js
@@ -1,9 +1,11 @@
 import { Container } from 'react-bootstrap';
+import Layout from './components/Layout/Layout';
 
 const App = () => {
 	return (
 		<Container>
-			<h2>Hello News</h2>
+			<h2 className="text-center text-success">React Bootstrap</h2>
+			<Layout />
 		</Container>
 	);
 };
--- 36-react-bootstrap-material-UI-axios-rechart/breaking-news/src/components/Header/Header.js
+++ 36-react-bootstrap-material-UI-axios-rechart/breaking-news/src/components/Header/Header.js
@@ -1,10 +1,16 @@
 import React from 'react';
-
+import { CardColumns, Card } from 'react-bootstrap';
 const Header = () => {
 	return (
-		<div>
-			<h2>Header</h2>
-		</div>
+		<CardColumns>
+			<Card>
+				<Card.Body>
+					<Card.Title>Home</Card.Title>
+					<Card.Title>Card title that wraps to a new line</Card.Title>
+					<Card.Text>This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</Card.Text>
+				</Card.Body>
+			</Card>
+		</CardColumns>
 	);
 };
 
