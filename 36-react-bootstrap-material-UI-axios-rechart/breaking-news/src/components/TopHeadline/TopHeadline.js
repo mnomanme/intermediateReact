3a8fd2ad80f1c5539539db8eeac72b9d4f551a6a
nomanme <abdullah.noman083@gmail.com>
2021-09-11 19:51:23 +0600

    load data

--- 36-react-bootstrap-material-UI-axios-rechart/breaking-news/src/components/TopHeadline/TopHeadline.js
+++ 36-react-bootstrap-material-UI-axios-rechart/breaking-news/src/components/TopHeadline/TopHeadline.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const TopHeadline = () => {
+	const [articles, setArticles] = useState([]);
+
+	useEffect(() => {
+		const url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=d0c56bba895e47d29a102669ef440db4`;
+		const fetchData = async () => {
+			try {
+				const res = await fetch(url);
+				const data = await res.json();
+				setArticles(data.articles);
+				console.log(data.articles);
+			} catch (error) {
+				console.log(error);
+			}
+		};
+		fetchData();
+	}, []);
+
 	return (
 		<div>
-			<h2>Top Headline</h2>
+			<h2>Top Headline: {articles.length}</h2>
 		</div>
 	);
 };
