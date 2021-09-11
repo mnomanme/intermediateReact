8a511ef7ccbf496e32ed5ed1aa33fafbd235f3b3
nomanme <abdullah.noman083@gmail.com>
2021-09-12 01:37:44 +0600

    show news

--- 36-react-bootstrap-material-UI-axios-rechart/test-material-ui/src/components/News/News.js
+++ 36-react-bootstrap-material-UI-axios-rechart/test-material-ui/src/components/News/News.js
@@ -17,18 +17,21 @@ const useStyles = makeStyles({
 	},
 });
 
-const News = () => {
+const News = (props) => {
 	const classes = useStyles();
+
+	const { title, urlToImage, description } = props.data;
+
 	return (
 		<Card className={classes.root}>
 			<CardActionArea>
-				<CardMedia className={classes.media} image="/static/images/cards/contemplative-reptile.jpg" title="Contemplative Reptile" />
+				<CardMedia className={classes.media} image={urlToImage} title="Contemplative Reptile" />
 				<CardContent>
-					<Typography gutterBottom variant="h5" component="h2">
-						Lizard
+					<Typography children gutterBottom variant="h5" component="h2">
+						{title}
 					</Typography>
 					<Typography variant="body2" color="textSecondary" component="p">
-						Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica
+						{description}
 					</Typography>
 				</CardContent>
 			</CardActionArea>
