7210d4cc96df5c0524e18c90a775eeefd8bf36d4
nomanme <abdullah.noman083@gmail.com>
2021-09-11 23:10:15 +0600

    show UI

--- 36-react-bootstrap-material-UI-axios-rechart/breaking-news/src/components/News/News.js
+++ 36-react-bootstrap-material-UI-axios-rechart/breaking-news/src/components/News/News.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Card, Button } from 'react-bootstrap';
 
-const News = () => {
+const News = (props) => {
+	console.log(props);
+	const { title, description, urlToImage, author } = props.article;
+
 	return (
-		<div>
-			<h2>News</h2>
-		</div>
+		<Card className="bg-info m-2">
+			<Card.Img variant="top" src={urlToImage} style={{ height: '400px' }} />
+			<Card.Header as="h3">{title}</Card.Header>
+			<Card.Body>
+				<Card.Title>Author: {author}</Card.Title>
+				<Card.Text>{description}</Card.Text>
+				<Button variant="warning">Go somewhere</Button>
+			</Card.Body>
+		</Card>
 	);
 };
 
