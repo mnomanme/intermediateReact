f6b1a6f210edb046befc6b1457081bf6c1ece048
nomanme <abdullah.noman083@gmail.com>
2021-09-12 02:05:33 +0600

    using axios

--- 36-react-bootstrap-material-UI-axios-rechart/test-material-ui/src/components/NewsSate/NewsState.js
+++ 36-react-bootstrap-material-UI-axios-rechart/test-material-ui/src/components/NewsSate/NewsState.js
@@ -1,23 +1,42 @@
 import React, { useEffect, useState } from 'react';
 import News from '../News/News';
+import axios from 'axios';
 
 const NewsState = () => {
 	const [article, setArticle] = useState([]);
 
+	// useEffect(() => {
+	// 	const url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=d0c56bba895e47d29a102669ef440db4`;
+
+	// 	const fetchData = async () => {
+	// 		try {
+	// 			const res = await fetch(url);
+	// 			const data = await res.json();
+	// 			setArticle(data.articles);
+	// 			console.log(data);
+	// 		} catch (error) {
+	// 			console.log(error);
+	// 		}
+	// 	};
+	// 	fetchData();
+	// }, []);
+
+	// axios
 	useEffect(() => {
 		const url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=d0c56bba895e47d29a102669ef440db4`;
 
 		const fetchData = async () => {
 			try {
-				const res = await fetch(url);
-				const data = await res.json();
-				setArticle(data.articles);
+				const data = await axios(url);
+				setArticle(data.data.articles);
 				console.log(data);
 			} catch (error) {
 				console.log(error);
 			}
 		};
 		fetchData();
+
+		// axios(url).then((data) => console.log(data));
 	}, []);
 
 	return (
