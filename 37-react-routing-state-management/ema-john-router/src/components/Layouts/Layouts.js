3d704cf0269069910d80de41735c1f9dba507e16
nomanme <abdullah.noman083@gmail.com>
2021-09-13 00:14:12 +0600

    set router path

--- 37-react-routing-state-management/ema-john-router/src/components/Layouts/Layouts.js
+++ 37-react-routing-state-management/ema-john-router/src/components/Layouts/Layouts.js
@@ -1,13 +1,34 @@
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import React from 'react';
 import Header from '../Header/Header';
 import Shop from '../Shop/Shop';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Review from '../Review/Review';
+import ManageInventory from '../ManageInventory/ManageInventory';
+import NotFound from '../NotFound/NotFound';
 
 const Layouts = () => {
 	return (
 		<>
 			<Header />
-			<Shop />
+			<Router>
+				<Switch>
+					<Route path="/shop">
+						<Shop />
+					</Route>
+					<Route path="/review">
+						<Review />
+					</Route>
+					<Route path="/inventory">
+						<ManageInventory />
+					</Route>
+					<Route exact path="/">
+						<Shop />
+					</Route>
+					<Route path="*">
+						<NotFound />
+					</Route>
+				</Switch>
+			</Router>
 		</>
 	);
 };
