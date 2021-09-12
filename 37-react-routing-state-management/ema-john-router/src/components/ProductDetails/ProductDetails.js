5c2703a7ebf6eef09f43192f7026fa4094024f16
nomanme <abdullah.noman083@gmail.com>
2021-09-13 02:06:53 +0600

    productdetails create

---
+++ 37-react-routing-state-management/ema-john-router/src/components/ProductDetails/ProductDetails.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+const ProductDetails = () => {
+	return (
+		<Container>
+			<h2>Product Details</h2>
+		</Container>
+	);
+};
+
+export default ProductDetails;
