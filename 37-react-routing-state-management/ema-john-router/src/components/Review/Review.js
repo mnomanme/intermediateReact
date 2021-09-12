aa59a3e24a85b37a7a85c8a88abdc4376dd8bf60
nomanme <abdullah.noman083@gmail.com>
2021-09-13 00:15:00 +0600

    new component create

---
+++ 37-react-routing-state-management/ema-john-router/src/components/Review/Review.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+const Review = () => {
+	return (
+		<Container>
+			<h2>This is review</h2>
+		</Container>
+	);
+};
+
+export default Review;
