82f2a009e4c9391a182a841a577c030b51eeb9cd
nomanme <abdullah.noman083@gmail.com>
2021-09-14 02:00:52 +0600

    review cart items length

--- 37-react-routing-state-management/ema-john-router/src/components/Review/Review.js
+++ 37-react-routing-state-management/ema-john-router/src/components/Review/Review.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container } from 'react-bootstrap';
+import fakeData from '../../resources/fakeData';
+import { getDatabaseCart } from '../../resources/utilities/databaseManager';
 
 const Review = () => {
+	const [cart, setCart] = useState([]);
+
+	useEffect(() => {
+		// cart
+		const storeCart = getDatabaseCart();
+		const productKeys = Object.keys(storeCart);
+		// const productsCount = productKeys.map((pdKey) => storeCart[pdKey]);
+		const productsCount = productKeys.map((pdKey) => {
+			const products = fakeData.find((data) => data.key === pdKey);
+			products.quantity = storeCart[pdKey];
+
+			return products;
+		});
+		setCart(productsCount);
+		console.log(productsCount);
+	}, []);
+
 	return (
 		<Container>
-			<h2>This is review</h2>
+			<h2>Order Items: {cart.length}</h2>
 		</Container>
 	);
 };
