0fb55bee45ada8dd5c0cc3758b5144d5ffa651d2
nomanme <abdullah.noman083@gmail.com>
2021-09-14 02:27:43 +0600

    pass components

--- 37-react-routing-state-management/ema-john-router/src/components/Review/Review.js
+++ 37-react-routing-state-management/ema-john-router/src/components/Review/Review.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Container } from 'react-bootstrap';
 import fakeData from '../../resources/fakeData';
 import { getDatabaseCart } from '../../resources/utilities/databaseManager';
+import ReviewItems from '../ReviewItems/ReviewItems';
 
 const Review = () => {
 	const [cart, setCart] = useState([]);
@@ -18,12 +19,16 @@ const Review = () => {
 			return products;
 		});
 		setCart(productsCount);
-		console.log(productsCount);
+		// console.log(productsCount);
 	}, []);
 
 	return (
 		<Container>
 			<h2>Order Items: {cart.length}</h2>
+			{cart.map((pd) => {
+				const { key } = pd;
+				return <ReviewItems key={key} reviewProduct={pd} />;
+			})}
 		</Container>
 	);
 };
