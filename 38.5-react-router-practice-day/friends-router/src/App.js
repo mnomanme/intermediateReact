e8cf91caea25796485427b9f50160718219cd51e
nomanme <abdullah.noman083@gmail.com>
2021-09-17 00:26:32 +0600

    update route

--- 38.5-react-router-practice-day/friends-router/src/App.js
+++ 38.5-react-router-practice-day/friends-router/src/App.js
@@ -1,22 +1,25 @@
+import { Container } from 'react-bootstrap';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import FriendsData from './components/FriendsData/FriendsData';
 import NotFound from './components/NotFound/NotFound';
 
 function App() {
 	return (
-		<Router>
-			<Switch>
-				<Route exact path="/">
-					<FriendsData></FriendsData>
-				</Route>
-				<Route path="/home">
-					<FriendsData></FriendsData>
-				</Route>
-				<Route path="*">
-					<NotFound />
-				</Route>
-			</Switch>
-		</Router>
+		<Container>
+			<Router>
+				<Switch>
+					<Route exact path="/">
+						<FriendsData></FriendsData>
+					</Route>
+					<Route path="/home">
+						<FriendsData></FriendsData>
+					</Route>
+					<Route path="*">
+						<NotFound />
+					</Route>
+				</Switch>
+			</Router>
+		</Container>
 	);
 }
 
