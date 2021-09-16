9115a7b3e89a4096d51d126fd7490274bbff6e1d
nomanme <abdullah.noman083@gmail.com>
2021-09-17 00:45:55 +0600

    show button add

--- 38.5-react-router-practice-day/friends-router/src/components/FriendsDetail/FriendsDetail.js
+++ 38.5-react-router-practice-day/friends-router/src/components/FriendsDetail/FriendsDetail.js
@@ -1,15 +1,23 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const FriendsDetail = (props) => {
-	const { name, email } = props.friend;
+	const { name, email, id } = props.friend;
 	console.log(props.friend);
 
 	return (
-		<section className="text-center bg-info m-5 p-5 rounded border border-warning">
-			<h2>Name :{name}</h2>
-			<p>Email: {email}</p>
-		</section>
+		<>
+			<section className="text-center bg-info m-5 p-5 rounded border border-warning">
+				<h2>Name :{name}</h2>
+				<p>Email: {email}</p>
+				<small>Id: {id}</small>
+				<br />
+				<Link to={`/friends/${id}`}>
+					<Button variant="success">Show Details</Button>
+				</Link>
+			</section>
+		</>
 	);
 };
 
