d0de61cb8e44330fd9067c6666130ef33e4ebb00
nomanme <abdullah.noman083@gmail.com>
2021-09-16 19:28:00 +0600

    place order

--- 38-more-router-react-devtool-conditional-format/ema-john-router/src/components/Review/Review.js
+++ 38-more-router-react-devtool-conditional-format/ema-john-router/src/components/Review/Review.js
@@ -1,13 +1,26 @@
 import React, { useEffect, useState } from 'react';
-import { Container } from 'react-bootstrap';
+import { Button, Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import fakeData from '../../resources/fakeData';
-import { getDatabaseCart, removeFromDatabaseCart } from '../../resources/utilities/databaseManager';
+import { getDatabaseCart, processOrder, removeFromDatabaseCart } from '../../resources/utilities/databaseManager';
 import Cart from '../Cart/Cart';
 import ReviewItems from '../ReviewItems/ReviewItems';
+import happyImage from '../../resources/images/giphy.gif';
 
 const Review = () => {
 	const [reviewCart, setReviewCart] = useState([]);
 
+	const [orderPlaced, setOrderPlaced] = useState(false);
+
+	// place order button handle
+	const handlePlaceOrder = () => {
+		console.log('order is now placed');
+		setReviewCart([]);
+		setOrderPlaced(true);
+		processOrder();
+	};
+
+	// remove product review item
 	const handleRemoveProduct = (productKey) => {
 		console.log('review removed', productKey);
 		const newCart = reviewCart.filter((pd) => {
@@ -32,6 +45,13 @@ const Review = () => {
 		// console.log(productsCount);
 	}, []);
 
+	// customer placeorder done happy image
+	let customerServiceDone;
+
+	if (orderPlaced) {
+		customerServiceDone = <img src={happyImage} alt="Order is being Placed" />;
+	}
+
 	return (
 		<Container className="d-flex">
 			<section className="productContainer">
@@ -40,9 +60,17 @@ const Review = () => {
 					const { key } = pd;
 					return <ReviewItems key={key} reviewProduct={pd} handleRemoveProduct={handleRemoveProduct} />;
 				})}
+				{/* order placed done */}
+				{customerServiceDone}
 			</section>
 			<section className="cartContainer">
-				<Cart cart={reviewCart} />
+				<Cart cart={reviewCart}>
+					<Link to="/review">
+						<Button onClick={handlePlaceOrder} variant="outline-info" size="md">
+							Place your Order
+						</Button>
+					</Link>
+				</Cart>
 			</section>
 		</Container>
 	);
