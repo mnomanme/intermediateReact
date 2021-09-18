a7bcb3cb48bc191f87cb9fb7ee2507821a507d41
nomanme <abdullah.noman083@gmail.com>
2021-09-19 01:35:15 +0600

    all team is work

--- 40-assignment/champions-league/src/Components/AllTeams/AllTeams.js
+++ 40-assignment/champions-league/src/Components/AllTeams/AllTeams.js
@@ -2,27 +2,25 @@ import React from 'react';
 import { Card, Col, Button } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowAltCircleRight } from '@fortawesome/free-solid-svg-icons';
+import './AllTeams.css';
 
 const AllTeams = (props) => {
 	const { strTeam, strTeamBadge, strSport, strCountry } = props.team;
 
 	return (
-		<>
-			<h4>This is All Teams</h4>
-			<Col lg={4} md={6} xs={12} className="teamColStyle">
-				<Card className="cardStyle">
-					<img className="logoStyle" variant="top" alt="team-badge" src={strTeamBadge} />
-					<Card.Body>
-						<h5>{strTeam}</h5>
-						<h6>Sports Type: {strSport}</h6>
-						<h6>Country: {strCountry}</h6>
-						<Button variant="primary">
-							Explore <FontAwesomeIcon icon={faArrowAltCircleRight} />
-						</Button>
-					</Card.Body>
-				</Card>
-			</Col>
-		</>
+		<Col lg={4} md={6} xs={12} className="Team">
+			<Card className="Card">
+				<img className="Logo" variant="top" alt="team-badge" src={strTeamBadge} />
+				<Card.Body>
+					<h5>{strTeam}</h5>
+					<h6>Sports Type: {strSport} </h6>
+					<h6>Country: {strCountry} </h6>
+					<Button variant="outline-success">
+						Explore <FontAwesomeIcon icon={faArrowAltCircleRight} />
+					</Button>
+				</Card.Body>
+			</Card>
+		</Col>
 	);
 };
 
