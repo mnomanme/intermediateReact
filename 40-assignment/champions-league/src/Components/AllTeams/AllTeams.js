5cb47d69fd59b183d34b4c620c0475af049dea90
nomanme <abdullah.noman083@gmail.com>
2021-09-19 01:43:01 +0600

    all team route

--- 40-assignment/champions-league/src/Components/AllTeams/AllTeams.js
+++ 40-assignment/champions-league/src/Components/AllTeams/AllTeams.js
@@ -3,9 +3,17 @@ import { Card, Col, Button } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowAltCircleRight } from '@fortawesome/free-solid-svg-icons';
 import './AllTeams.css';
+import { useHistory } from 'react-router';
 
 const AllTeams = (props) => {
 	const { strTeam, strTeamBadge, strSport, strCountry } = props.team;
+
+	const teamHistory = useHistory();
+
+	const showTeamDetail = (strTeam) => {
+		const url = `/tDetails/${strTeam}`;
+		teamHistory.push(url);
+	};
 
 	return (
 		<Col lg={4} md={6} xs={12} className="Team">
@@ -15,7 +23,7 @@ const AllTeams = (props) => {
 					<h5>{strTeam}</h5>
 					<h6>Sports Type: {strSport} </h6>
 					<h6>Country: {strCountry} </h6>
-					<Button variant="outline-success">
+					<Button onClick={() => showTeamDetail(strTeam)} variant="outline-success">
 						Explore <FontAwesomeIcon icon={faArrowAltCircleRight} />
 					</Button>
 				</Card.Body>
