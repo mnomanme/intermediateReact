6c0886c57b10a9b3c2244a334f2f2cc357a3baad
nomanme <abdullah.noman083@gmail.com>
2021-09-18 23:57:10 +0600

    card style & icon add

--- 40-assignment/champions-league/src/Components/TeamDetails/TeamDetails.js
+++ 40-assignment/champions-league/src/Components/TeamDetails/TeamDetails.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import { Card, Col, Button } from 'react-bootstrap';
+import './TeamDetails.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowAltCircleRight } from '@fortawesome/free-solid-svg-icons';
 
 const TeamDetails = (props) => {
 	console.log(props.team);
 	const { strCountry, strTeam, strSport, strTeamBadge } = props.team;
+
 	return (
-		<Col lg={4} md={6} xs={12} className="teamColStyle">
-			<Card className="cardStyle">
-				<img className="logoStyle" variant="top" alt="team-badge" src={strTeamBadge} />
+		<Col lg={4} md={6} xs={12} className="Team">
+			<Card className="Card">
+				<img className="Logo" variant="top" alt="team-badge" src={strTeamBadge} />
 				<Card.Body>
 					<h5>{strTeam}</h5>
 					<h6>Sports Type: {strSport} </h6>
 					<h6>Country: {strCountry} </h6>
-					<Button variant="primary">Explore </Button>
+					<Button variant="outline-success">
+						Explore <FontAwesomeIcon icon={faArrowAltCircleRight} />
+					</Button>
 				</Card.Body>
 			</Card>
 		</Col>
