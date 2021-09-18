398762bdd7da31f355962aadf3abd593e78880a7
nomanme <abdullah.noman083@gmail.com>
2021-09-18 22:42:10 +0600

    update

--- 39.5-react-router-practice/social-buddy/src/components/Home/Home.js
+++ 39.5-react-router-practice/social-buddy/src/components/Home/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
-import Post from '../../Post/Post';
+import Post from '../Post/Post';
 
 const Home = () => {
 	const [post, setPost] = useState([]);
