44230763144049540399dd5cb8d574b137562ff6
nomanme <abdullah.noman083@gmail.com>
2021-09-19 01:14:34 +0600

    home done

--- 40-assignment/champions-league/src/Components/Home/Home.js
+++ 40-assignment/champions-league/src/Components/Home/Home.js
@@ -1,44 +1,13 @@
-import React, { useEffect, useState } from 'react';
-import { Container, Row } from 'react-bootstrap';
+import React from 'react';
 import Header from '../Header/Header';
-import TeamDetails from '../TeamDetails/TeamDetails';
+import Teams from '../Teams/Teams';
 
 const Home = () => {
 	return (
 		<>
+			<h2>This is Home</h2>
 			<Header />
-			<Team />
-		</>
-	);
-};
-
-const Team = () => {
-	const [team, setTeam] = useState([]);
-
-	useEffect(() => {
-		try {
-			const fetchData = async () => {
-				const url = `https://www.thesportsdb.com/api/v1/json/1/lookup_all_teams.php?id=4490`;
-				const res = await fetch(url);
-				const data = await res.json();
-				// console.log(data.teams);
-				setTeam(data.teams);
-			};
-			fetchData();
-		} catch (error) {
-			console.log(error);
-		}
-	}, []);
-
-	return (
-		<>
-			<Container>
-				<Row>
-					{team.map((team) => (
-						<TeamDetails key={team.idTeam} team={team}></TeamDetails>
-					))}
-				</Row>
-			</Container>
+			<Teams />
 		</>
 	);
 };
