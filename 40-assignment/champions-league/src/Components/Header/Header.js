0b72d29454c2af6085aa5adc1250565f89824a14
nomanme <abdullah.noman083@gmail.com>
2021-09-18 23:16:02 +0600

    header add

---
+++ 40-assignment/champions-league/src/Components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+	return (
+		<>
+			<section className="Header">
+				<h2 className="text-light fw-bolder">Champions League 2021</h2>
+			</section>
+		</>
+	);
+};
+
+export default Header;
