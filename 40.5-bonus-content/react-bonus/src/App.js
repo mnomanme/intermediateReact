7853c2dc575c6cfc649950ac02b1073ca9a1c990
nomanme <abdullah.noman083@gmail.com>
2021-09-20 23:29:02 +0600

    nested data

--- 40.5-bonus-content/react-bonus/src/App.js
+++ 40.5-bonus-content/react-bonus/src/App.js
@@ -1,19 +1,54 @@
 import './App.css';
 import ThumbUpIcon from '@mui/icons-material/ThumbUp';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
 	const [likeColor, setLikeColor] = useState('');
+	const [users, setUsers] = useState([]);
+	const [singleUser, setSingleUser] = useState({});
+	const [randomUser, setRandomUser] = useState([]);
 
 	const handleLike = () => {
 		const like = likeColor ? '' : 'primary';
 		setLikeColor(like);
 	};
 
+	useEffect(() => {
+		try {
+			async function fetchData() {
+				const url = `https://jsonplaceholder.typicode.com/users`;
+				const res = await fetch(url);
+				const data = await res.json();
+				console.log(data);
+				setUsers(data);
+			}
+			fetchData();
+		} catch (error) {
+			console.log(error);
+		}
+		// single user
+		fetch(`https://jsonplaceholder.typicode.com/users/1`)
+			.then((res) => res.json())
+			.then((data) => setSingleUser(data));
+		// random user
+		fetch(`https://randomuser.me/api/`)
+			.then((res) => res.json())
+			.then((data) => setRandomUser(data.results[0]));
+		//
+	}, []);
+
 	return (
 		<div className="App">
-			<h2>Hello React</h2>
+			<h2>Hello React </h2>
+
 			<ThumbUpIcon onClick={handleLike} fontSize="large" color={likeColor}></ThumbUpIcon>
+
+			{users.map((users) => (
+				<li key={users.id}>{users.name}</li>
+			))}
+
+			<h3>Single Name: {singleUser.name}</h3>
+			<h3>Random: {randomUser.name?.title} </h3>
 		</div>
 	);
 }
